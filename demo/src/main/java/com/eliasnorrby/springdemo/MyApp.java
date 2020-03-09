4b96d5e1f7feb33a3b21be03e271462b2e103e85
Elias Norrby <elias.norrby@gmail.com>
2020-03-09 17:09:09 +0100

    L19: Code Demo - Rough Prototype Part 1

---
+++ demo/src/main/java/com/eliasnorrby/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.eliasnorrby.springdemo;
+
+public class MyApp {
+  public static void main(String[] args) {
+    // create the object
+    Coach theCoach = new BaseballCoach();
+
+    // use the object
+    System.out.println(theCoach.getDailyWorkout());
+  }
+}
