9a2f18a8a103604deb011e33ce446254cd2bfbe3
Elias Norrby <elias.norrby@gmail.com>
2020-03-12 11:23:38 +0100

    L32: Spring Dependency Injection - Write Some Code - Part 3

--- demo/src/main/java/com/eliasnorrby/springdemo/TrackCoach.java
+++ demo/src/main/java/com/eliasnorrby/springdemo/TrackCoach.java
@@ -3,6 +3,8 @@ package com.eliasnorrby.springdemo;
 public class TrackCoach implements Coach {
   private FortuneService fortuneService;
 
+  public TrackCoach() {}
+
   public TrackCoach(FortuneService fortuneService) {
     this.fortuneService = fortuneService;
   }
@@ -14,6 +16,6 @@ public class TrackCoach implements Coach {
 
   @Override
   public String getDailyFortune() {
-    return fortuneService.getFortune();
+    return "Just Do It: " + fortuneService.getFortune();
   }
 }
