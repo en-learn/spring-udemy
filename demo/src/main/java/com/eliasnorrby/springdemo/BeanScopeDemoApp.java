514eb34c24e8598b6edf36353bb25539aa412597
Elias Norrby <elias.norrby@gmail.com>
2020-03-12 15:12:23 +0100

    L45: Bean Scopes - Write Some Code - Part 2

--- demo/src/main/java/com/eliasnorrby/springdemo/BeanScopeDemoApp.java
+++ demo/src/main/java/com/eliasnorrby/springdemo/BeanScopeDemoApp.java
@@ -7,7 +7,21 @@ public class BeanScopeDemoApp {
     ClassPathXmlApplicationContext context =
         new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
+    // retrieve bean from spring container
     Coach theCoach = context.getBean("myCoach", Coach.class);
     Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+    // check if they are the same
+    boolean areTheSame = (theCoach == alphaCoach);
+
+    // print out the results
+    System.out.println("\nPointing to the same object: " + areTheSame);
+
+    System.out.println("\nMemory location for theCoach: " + theCoach);
+
+    System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+    // close the context
+    context.close();
   }
 }
